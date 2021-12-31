f23cad272de0f6977928d8210603597a21dcc226
QueenTesa Fuggett <qfuggett@gmail.com>
2021-12-31 10:26:17 -0500

    created filter functionality to display specific robots on search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
 import { robots } from './Robots';
 import './App.css';
 
-const App = () => {
-    return (
-      <div className='tc'>
-        <h1>Robofriends</h1>
-        <SearchBox />
-        <Cardlist robots={robots}/>
-      </div>
-    );
+
+class App extends Component {
+
+  //An object that declares the state
+  constructor() {
+    //Calls the constructor
+    super()
+    //An object that describes your application, that can change and affect your app - lives in the parent component
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+
+  onSearchChange = (event) => {
+    //sets the state within this object by assigning it values
+    this.setState({ searchfield: event.target.value})
+  }
+
+    render () {
+
+      const filteredRobots = this.state.robots.filter(robot => {
+        //only return the robots whose names match the search field
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      })
+
+      return (
+        <div className='tc'>
+          <h1>Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Cardlist robots={filteredRobots}/>
+        </div>
+      );
+      }
 }
 
 export default App;
