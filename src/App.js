342b74f756a9f46ccab250edb30bd52641a2619d
QueenTesa Fuggett <qfuggett@gmail.com>
2022-01-02 20:48:06 -0500

    added scrollable feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -44,7 +45,9 @@ class App extends Component {
             <div className='tc'>
               <h1 className='f1'>Robofriends</h1>
               <SearchBox searchChange={this.onSearchChange}/>
-              <Cardlist robots={filteredRobots}/>
+              <Scroll>
+                <Cardlist robots={filteredRobots}/>
+              </Scroll>
             </div>
           );
       }
