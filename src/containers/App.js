8b255121c8ba52306659944c124f2ce95d49fc2d
QueenTesa Fuggett <qfuggett@gmail.com>
2022-01-05 00:41:09 -0500

    converted class components to functional, implemented hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Cardlist from '../components/Cardlist';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,55 +6,39 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
-class App extends Component {
-//Considered a smart component because it has state
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  //An object that declares the state
-  constructor() {
-    //Calls the constructor
-    super()
-    //An object that describes your application, that can change and affect your app - lives in the parent component
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users}));
-  }
-
-
-  onSearchChange = (event) => {
+  const onSearchChange = (event) => {
     //sets the state within this object by assigning it values
-    this.setState({ searchfield: event.target.value})
+    setSearchfield(event.target.value)
   }
 
-    render () {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {setRobots(users)});
+  },[]) //only run useEffect when this value changes; acts similarly to componentDidMount if empty
 
-      const { robots, searchfield } = this.state;
+  const filteredRobots = robots.filter(robot => {
+    //only return the robots whose names match the search field
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
 
-      const filteredRobots = robots.filter(robot => {
-        //only return the robots whose names match the search field
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-      })
-
-      return !robots.length ?
-        <h1>Loading...</h1> :
-        (
-          <div className='tc'>
-            <h1 className='f1'>Robofriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-              <ErrorBoundary>
-                <Cardlist robots={filteredRobots}/>
-              </ErrorBoundary>
-            </Scroll>
-          </div>
-        );
-    }
+  return !robots.length ?
+    <h1>Loading...</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>Robofriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <ErrorBoundary>
+            <Cardlist robots={filteredRobots}/>
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );  
 }
 
 export default App;
